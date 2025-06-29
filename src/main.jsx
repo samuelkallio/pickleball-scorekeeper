c095d1d0cbddc92ba94df98ef99f09679f6e2ba2
samuelkallio <159822903+samuelkallio@users.noreply.github.com>
2025-06-29 10:40:51 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'App.js';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
